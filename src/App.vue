8a0e6b1ba9f19165ea8e99521566e0a810b7e32f
SukanyaNam <kjjj5406@email.com>
2024-01-09 12:09:48 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <router-link :to= "{name: 'Home'}">Home</router-link>
+    <router-link :to="{name:'About'}">About</router-link>
+  </div>
+  <router-view />
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
